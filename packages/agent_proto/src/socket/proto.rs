72794b511a92486309c5f98f5cc8c73cdf118638
Giuseppe Eletto <giuseppe@eletto.me>
2023-02-26 17:47:02 +0100

    feat(proto): impl traits in socket module

--- packages/agent_proto/src/socket/proto.rs
+++ packages/agent_proto/src/socket/proto.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
 pub enum Protocol {
     #[serde(rename = "tcp")]
     Tcp,
