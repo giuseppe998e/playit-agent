8162a45667623b9c49f33f7f59c8d71f34c4fa22
Giuseppe Eletto <giuseppe@eletto.me>
2023-02-28 22:35:32 +0100

    chore(proto): update tokio encode tests

--- packages/agent_proto/src/tokio/mod.rs
+++ packages/agent_proto/src/tokio/mod.rs
@@ -3,12 +3,6 @@ pub mod en;
 
 #[cfg(test)]
 mod en_dec {
-    macro_rules! aw {
-        ($e:expr) => {
-            tokio_test::block_on($e)
-        };
-    }
-
     use std::{
         io::Cursor,
         mem::size_of,
@@ -29,8 +23,8 @@ mod en_dec {
         socket::{Port, Protocol, Socket, SocketFlow, SocketFlowV4, SocketFlowV6, V6_BYTES},
     };
 
-    #[test]
-    fn test_agentsession() {
+    #[tokio::test]
+    async fn test_agentsession() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<AgentSession>());
         let data = AgentSession {
             id: random(),
@@ -39,16 +33,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(AgentSession::read_from(&mut buf_cursor));
+        let dec_result = AgentSession::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_controlrequest() {
+    #[tokio::test]
+    async fn test_controlrequest() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<ControlRequest>());
         let data = ControlRequest::KeepAlive(KeepAliveRequest {
             id: random(),
@@ -57,30 +51,30 @@ mod en_dec {
         });
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(ControlRequest::read_from(&mut buf_cursor));
+        let dec_result = ControlRequest::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_controlresponse() {
+    #[tokio::test]
+    async fn test_controlresponse() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<ControlResponse>());
         let data = ControlResponse::RequestQueued;
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(ControlResponse::read_from(&mut buf_cursor));
+        let dec_result = ControlResponse::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_ping() {
+    #[tokio::test]
+    async fn test_control_ping() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<Ping>());
         let data = Ping {
             now: random(),
@@ -88,16 +82,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(Ping::read_from(&mut buf_cursor));
+        let dec_result = Ping::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_pong() {
+    #[tokio::test]
+    async fn test_control_pong() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<Pong>());
         let data = Pong {
             request_now: random(),
@@ -110,16 +104,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(Pong::read_from(&mut buf_cursor));
+        let dec_result = Pong::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_portmappingrequest() {
+    #[tokio::test]
+    async fn test_control_portmappingrequest() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<PortMappingRequest>());
         let data = PortMappingRequest {
             session: AgentSession {
@@ -135,16 +129,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(PortMappingRequest::read_from(&mut buf_cursor));
+        let dec_result = PortMappingRequest::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_portmappingresponse() {
+    #[tokio::test]
+    async fn test_control_portmappingresponse() {
         let rand = random();
 
         let mut buf = Vec::<u8>::with_capacity(size_of::<PortMappingResponse>());
@@ -158,16 +152,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(PortMappingResponse::read_from(&mut buf_cursor));
+        let dec_result = PortMappingResponse::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_portmappingfound() {
+    #[tokio::test]
+    async fn test_control_portmappingfound() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<PortMappingFound>());
         let data = PortMappingFound::ToAgent(AgentSession {
             id: random(),
@@ -176,16 +170,16 @@ mod en_dec {
         });
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(PortMappingFound::read_from(&mut buf_cursor));
+        let dec_result = PortMappingFound::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_registerrequest() {
+    #[tokio::test]
+    async fn test_control_registerrequest() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<RegisterRequest>());
         let data = RegisterRequest {
             account_id: random(),
@@ -198,16 +192,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(RegisterRequest::read_from(&mut buf_cursor));
+        let dec_result = RegisterRequest::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_registerresponse() {
+    #[tokio::test]
+    async fn test_control_registerresponse() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<RegisterResponse>());
         let data = RegisterResponse {
             session: AgentSession {
@@ -219,16 +213,16 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(RegisterResponse::read_from(&mut buf_cursor));
+        let dec_result = RegisterResponse::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_control_udpchanneldetails() {
+    #[tokio::test]
+    async fn test_control_udpchanneldetails() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<UdpChannelDetails>());
         let data = UdpChannelDetails {
             tunnel_addr: SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, random())),
@@ -236,11 +230,11 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(UdpChannelDetails::read_from(&mut buf_cursor));
+        let dec_result = UdpChannelDetails::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
@@ -249,8 +243,8 @@ mod en_dec {
     //      // NOT_NEEDED alias of "AgentSession"
     // }
 
-    #[test]
-    fn test_hmacsign() {
+    #[tokio::test]
+    async fn test_hmacsign() {
         use sha2::digest::KeyInit;
         let hmac = hmac::Hmac::<sha2::Sha256>::new_from_slice(&random::<[u8; 16]>()).unwrap();
 
@@ -258,16 +252,16 @@ mod en_dec {
         let data = hmac.sign_data(&random::<[u8; 16]>());
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(HmacSign::<sha2::Sha256>::read_from(&mut buf_cursor));
+        let dec_result = HmacSign::<sha2::Sha256>::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_socket() {
+    #[tokio::test]
+    async fn test_socket() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<Socket>());
         let data = Socket {
             ip: IpAddr::V4(Ipv4Addr::UNSPECIFIED),
@@ -276,44 +270,44 @@ mod en_dec {
         };
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(Socket::read_from(&mut buf_cursor));
+        let dec_result = Socket::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_socket_port() {
+    #[tokio::test]
+    async fn test_socket_port() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<Port>());
         let data = Port::Single(random());
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(Port::read_from(&mut buf_cursor));
+        let dec_result = Port::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_socket_proto() {
+    #[tokio::test]
+    async fn test_socket_proto() {
         let mut buf = Vec::<u8>::with_capacity(size_of::<Protocol>());
         let data = Protocol::Both;
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(Protocol::read_from(&mut buf_cursor));
+        let dec_result = Protocol::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_socketflow_v4() {
+    #[tokio::test]
+    async fn test_socketflow_v4() {
         let mut buf = Vec::<u8>::with_capacity(V6_BYTES + 1);
         let data = SocketFlow::V4(SocketFlowV4::new(
             SocketAddrV4::new([192, 168, 1, 1].into(), 1324),
@@ -321,16 +315,16 @@ mod en_dec {
         ));
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(SocketFlow::read_from(&mut buf_cursor));
+        let dec_result = SocketFlow::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 
-    #[test]
-    fn test_socketflow_v6() {
+    #[tokio::test]
+    async fn test_socketflow_v6() {
         let mut buf = Vec::<u8>::with_capacity(V6_BYTES + 1);
         let data = SocketFlow::V6(SocketFlowV6::new(
             SocketAddrV6::new([192, 168, 1, 1, 1, 255, 1, 1].into(), 1324, 6543, 0),
@@ -339,11 +333,11 @@ mod en_dec {
         ));
 
         // Encode
-        assert!(matches!(aw!(data.clone().write_into(&mut buf)), Ok(_)));
+        assert!(matches!(data.clone().write_into(&mut buf).await, Ok(_)));
 
         // Decode
         let mut buf_cursor = Cursor::new(buf);
-        let dec_result = aw!(SocketFlow::read_from(&mut buf_cursor));
+        let dec_result = SocketFlow::read_from(&mut buf_cursor).await;
         assert_eq!(data, dec_result.unwrap())
     }
 }
